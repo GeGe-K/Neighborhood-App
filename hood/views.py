9ec755b1a731564f08abef670e6ac047fd7b775f
MaryannGitonga <naygitonga4@gmail.com>
2018-11-24 11:19:38 +0300

    Add change neighborhood view function

--- hood/views.py
+++ hood/views.py
@@ -103,3 +103,11 @@ def add_biz(request):
     else:
         business_form = AddBusinessForm()
     return render(request,'biz.html',{'business_form':business_form})
+
+
+def change_neighborhood(request,neighborhood_id):
+    profile = UserProfile.objects.filter(user = request.user).first()
+    neighborhood = Neighborhood.objects.get(id=neighborhood_id)
+    profile.neighborhood = neighborhood
+    profile.save()
+    return redirect(reverse('neighborhood',args=[neighborhood.id]))
