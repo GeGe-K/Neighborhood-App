84e0223839c5a2732b530de691dd1544f7b5bfdb
MaryannGitonga <naygitonga4@gmail.com>
2018-11-24 11:21:59 +0300

    Add change neighborhood url pattern

--- hood/urls.py
+++ hood/urls.py
@@ -7,5 +7,6 @@ urlpatterns=[
     url(r'^neighborhood/(\d+)',views.neighborhood,name='neighborhood'),
     url(r'^profile/(\d+)',views.profile,name='profile'),
     url(r'^add_biz/',views.add_biz,name='add_biz'),
+    url(r'^change_neighborhood/(\d+)',views.change_neighborhood,name='change_neighborhood'),
     
 ]
