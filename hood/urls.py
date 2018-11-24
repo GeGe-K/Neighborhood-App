c62c5cf41b1648ea31228f7e4a85e58af4f6b506
MaryannGitonga <naygitonga4@gmail.com>
2018-11-24 11:21:01 +0300

    Add neighborhood url pattern

--- hood/urls.py
+++ hood/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
     url(r'^$',views.index, name='index'),
     url(r'^signout/$',views.signout,name='signout'),
+    url(r'^neighborhood/(\d+)',views.neighborhood,name='neighborhood'),
     
-]
