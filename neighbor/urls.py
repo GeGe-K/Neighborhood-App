71c7d65568eeff231d28171f416e1c6c1f741af6
MaryannGitonga <naygitonga4@gmail.com>
2018-11-24 11:16:19 +0300

    Add urls for django-registration

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -20,5 +20,8 @@ from django_registration.backends.one_step.views import RegistrationView
 urlpatterns = [
     url(r'^admin/', admin.site.urls,name='admin'),
     url(r'',include('hood.urls')),
+    url(r'^accounts/register/',RegistrationView.as_view(success_url='/accounts/login/'),name='django_registration_register'),
+    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     
 ]
